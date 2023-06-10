3de371231ed77b044a4a26f275c3b91b1529ad2d
akunke2selfiah <133901908+akunke2selfiah@users.noreply.github.com>
2023-06-10 11:02:03 +0700

    11

--- Assets/script/jawabsoal.cs
+++ Assets/script/jawabsoal.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f230953b94cfb60642f8954a31d13335cc5a07ee990f43e4edc4616f2f201025
-size 2275
+oid sha256:5b8da1da75a1190a13778cf1c9a926aa960248eb7b27293f7dc05edf08e088dc
+size 789
